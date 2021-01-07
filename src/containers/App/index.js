8be76e152209d73ba34869603e929f60f037f21e
ayoubbahmad <ayoubbahmad@gmail.com>
2021-01-07 23:04:44 +0100

    clean project and create files struct

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>hello</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
